9f40a8fe632a7633de65cfca40c0f813f59bbc45
Jonny Haynes <jonny@colouringcode.com>
2016-06-13 13:47:22 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var strava = require('strava-v3'),
+    express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  strava.athlete.listActivities({id:627105},function(err, activities) {
+    if(!err) {
+      res.render('index', {
+        activities: activities
+      });
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.get('/:id', function(req, res) {
+  strava.streams.activity({id: req.params.id, types: 'time, latlng, distance, altitude, heartrate, cadence, watts, temp', resolution: 'medium'},function(err, activity) {
+    if(!err) {
+      res.send(activity);
+      //res.render('activities');
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function() {
+  console.info('Express app started on ' + app.get('port'));
+});
