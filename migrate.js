4f5e40866f29a8be9207bd675ba6543514ca6809
Jonny Haynes <jonny@colouringcode.com>
2016-12-14 10:04:14 +0000

    WIP - DB added, DB Setup script, adding auth tokens to DB after member check

---
+++ migrate.js
@@ -0,0 +1,9 @@
+const sqlite3 = require('sqlite3').verbose();
+
+var db = new sqlite3.Database('sitwellccstats.db');
+
+db.serialize(function() {
+  db.run("CREATE TABLE members (accessToken TEXT)");
+});
+
+db.close();
